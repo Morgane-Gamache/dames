d90f5e45842c16ecbca33616fc9df10c9025655b
Morgane <2374332@cstjean.qc.ca>
2026-09-10 11:59:17 -0400

    partie 4

--- tests/cstjean/mobile/pion/TestPion.java
+++ tests/cstjean/mobile/pion/TestPion.java
@@ -2,9 +2,9 @@ package cstjean.mobile.pion;
 import junit.framework.TestCase;
 public class TestPion extends TestCase {
     public void testCreer() {
-        Pion pionA = new Pion("rouge");
+        Pion pionA = new Pion("blanc");
         Pion pionB = new Pion("noir");
-        assertEquals("rouge", pionA.getCouleur());
+        assertEquals("blanc", pionA.getCouleur());
         assertEquals("noir", pionB.getCouleur());
     }
 }
